54a8e615c43ae1d04e4be80854c1b0760d1d02cd
Thomas J Bradley <theman@thomasjbradley.ca>
2010-11-12 13:39:16 -0500

    Added an example signature to text using journal.ttf font.

---
+++ signature-to-text.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Content-type: image/png');
+
+$img = imagecreatetruecolor(400, 30);
+
+$bgColour = imagecolorallocate($img, 0xff, 0xff, 0xff);
+$penColour = imagecolorallocate($img, 0x14, 0x53, 0x94);
+imagefilledrectangle($img, 0, 0, 399, 29, $bgColour);
+
+$text = 'Testing...';
+$font = 'journal.ttf';
+
+imagettftext($img, 20, 0, 10, 20, $penColour, $font, $text);
+
+imagepng($img);
+imagedestroy($img);
